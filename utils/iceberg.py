f734a698886b68c02579b24c524f5c3dab123132
Abhay Singh <sinnghabhay80@gmail.com>
2025-12-13 22:53:20 +0530

    using SparkSession from caller session instead of new

--- utils/iceberg.py
+++ utils/iceberg.py
@@ -1,7 +1,8 @@
 from pyspark.sql import DataFrame
-from utils.spark import get_spark_session
 from utils.logger import get_logger
 from utils.config import get_project_root
+
+from pyspark.sql import SparkSession
 
 logger = get_logger(__name__)
 
@@ -11,8 +12,7 @@ def read_sql_from_path(path):
     with open(full_path) as f:
         return f.read()
 
-def create_iceberg_table(sql_path: str):
-    spark = get_spark_session()
+def create_iceberg_table(sql_path: str, spark: SparkSession):
     sql = read_sql_from_path(sql_path)
     logger.info(f"Creating iceberg table: {sql.split()[5]}")
     spark.sql(sql)
