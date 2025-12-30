9bbfa487b847a20313a8f5e34750f2383c7f5506
Abhay Singh <sinnghabhay80@gmail.com>
2025-12-30 02:01:21 -0800

    Add Bronze -> Silver transformation for Sales data

---
+++ scripts/transformation/bronze_to_silver/sales.py
@@ -0,0 +1,32 @@
+from utils.spark import get_spark_session
+from utils.logger import get_logger
+from utils.iceberg import create_iceberg_table, write_iceberg_table
+from pyspark.sql.functions import col, round, to_date, lit
+
+logger = get_logger(__name__)
+
+def main():
+    spark = get_spark_session()
+    source_table = "hive_iceberg.bronze.sales"
+    target_table = "hive_iceberg.silver.sales"
+    ddl_path = "sql/schema/silver/sales.sql"
+
+    logger.info(f"Transforming {source_table} → {target_table}...")
+    create_iceberg_table(ddl_path, spark)
+
+    bronze = spark.table(source_table)
+    silver = bronze.filter((col("quantity") > 0) & (col("price") > 0)) \
+        .withColumn("total_amount", round(col("quantity") * col("price"), 2)) \
+        .withColumn("order_date", to_date("order_date"))  \
+        .withColumn("is_valid", lit(True)) \
+        .select(
+            "order_id", "customer_id", "product_id", "quantity", "price",
+            "total_amount", "order_date", "is_valid", "_ingestion_time"
+        )
+
+    write_iceberg_table(silver, target_table)
+    logger.info(f"Silver table {target_table} created → {silver.count():,} valid rows")
+
+
+if __name__ == "__main__":
+    main()
--- sql/schema/silver/sales.sql
+++ sql/schema/silver/sales.sql
@@ -1,10 +1,12 @@
-CREATE TABLE IF NOT EXISTS hive_iceberg.bronze.sales (
+CREATE TABLE IF NOT EXISTS hive_iceberg.silver.sales (
     order_id STRING,
     customer_id STRING,
     product_id STRING,
     quantity DOUBLE,
     price DOUBLE,
-    order_date TIMESTAMP,
+    total_amount DOUBLE,
+    order_date DATE,
+    is_valid BOOLEAN,
     _ingestion_time TIMESTAMP
 ) USING iceberg
 PARTITIONED BY (months(order_date));
