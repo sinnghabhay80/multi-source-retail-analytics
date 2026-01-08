021c5bab3eac8cedbef6894b10da69fef7c03b49
Abhay Singh <sinnghabhay80@gmail.com>
2026-01-08 04:41:03 -0800

    Update logger for better visibility[Fixed]

--- utils/logger.py
+++ utils/logger.py
@@ -4,7 +4,7 @@ def get_logger(name: str):
     logger = logging.getLogger(name)
     if not logger.handlers:
         handler = logging.StreamHandler()
-        formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+        formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(logging.INFO)
